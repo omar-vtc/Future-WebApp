c5ffee55e1b1c80c59bf898367e489417fd35e35
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-07-07 10:34:02 +0300

    added the goals group box

--- future-next-app/components/organisms/GoalsSection.tsx
+++ future-next-app/components/organisms/GoalsSection.tsx
@@ -1,12 +1,28 @@
 import BoxWithImage from "@components/molecules/BoxWithImage";
 import SectionHeader from "@components/molecules/SectionHeader";
+import Box from "@mui/material/Box";
+
 import React from "react";
 
 function GoalsSection() {
   return (
     <>
       <SectionHeader title="Our Goals" variant="h3" />
-      <BoxWithImage />
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          p: 3,
+          //   border: "solid red",
+          gap: 8,
+        }}
+      >
+        <BoxWithImage />
+        <BoxWithImage />
+        <BoxWithImage />
+        <BoxWithImage />
+      </Box>
     </>
   );
 }
