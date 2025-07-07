2bec85a1458e8c907c04af93ed39f1beb4a71f37
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-07-07 04:11:18 +0300

    import

--- future-next-app/app/home/page.tsx
+++ future-next-app/app/home/page.tsx
@@ -1,6 +1,6 @@
 import ImageCarousel from "@components/organisims/ImageCarousel";
 import React from "react";
-import AboutSection from "@components/organisims/AboutSection";
+import AboutSection from "../../components/organisims/AboutSection";
 
 function page() {
   return (
